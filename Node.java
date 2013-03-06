931f5ed56f676ddcec04c4aa292c0bf73c050f68
Colin Quick <quickcj@s.dscdk12.org>
2013-03-06 12:47:08 -0700

    created constructors and getters and setters

--- Node.java
+++ Node.java
@@ -7,5 +7,39 @@
 
 public class Node {
 
+	private int value;
+	private Node next;
+
+	/**
+	 * Constructors      
+	 */			
+	public Node(int value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
+
+	public Node() {
+		this(0, null);
+	}
+
+	/**
+	 * Getters and Setters
+	 */
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
 
 }
