e971030cecc3572301e7f2409e5f36b31d2a9bb1
Colin Quick <quickcj@s.dscdk12.org>
2013-03-12 08:23:23 -0600

    finished push

--- LinkedList.java
+++ LinkedList.java
@@ -4,22 +4,30 @@
  * 		a linked list of nodes that contain integer values and a link to another node
  */
 
-public class LinkedList {
-	private Node[] nodes;
+public class LinkedList extends Node {
 	private Node head;
-
+/**
+ * Constructors
+ * 
+ */
+	public LinkedList(Node head) {
+		this.head = head;
+	}
 	public LinkedList() {
+		this(null);
+	}
+
+	public static void main(String[] args) {
 
 	}
 	/**
 	 * Push Method
 	 *
-	 * 		creates a node and linds it to the node the head is linked to then links the head to itself
+	 * 		creates a node and links it to the node the head is linked to then links the head to itself
 	 */
 	public void push() {
-		Node a = null;
-		a.next = head.next;
-		head.next = a;
+		Node a = new Node(7, this.head);
+		this.head.setNext(a);
 	}
 	/**
  	* length		
@@ -28,16 +36,23 @@ public class LinkedList {
  	*
  	* @param	given an array of nodes
  	*/
-	public int length(Node[] nodes) {
-		return nodes.length;
+	public int length() {
+		return 0;
 	}
-
+	/**
+	 * buildOneTwoThree
+	 *
+	 * 		sets the value of the LinkedList to {1,2,3}
+	 *
+	 * @param	takes an array of nodes
+	 * return - returns a list of nodes of length 3
+	 */
 	public void buildOneTwoThree() {
-
+		
 	}
 
 	public int countNumbers(int value) {
-
+		return 0;
 	}
 
 }
